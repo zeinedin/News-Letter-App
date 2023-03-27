25ff19f8f2f3eb2ed357f3c1d8350a54a6dabefe
zeinedin <78230325+zeinedin@users.noreply.github.com>
2023-03-27 08:37:32 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const body = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(body.urlencoded({extended: true}));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", (req, res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const https = require("https")
+    const data = {
+        members:[{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+             }   
+            ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us17.api.mailchimp.com/3.0/lists/5cc58dd8a9"
+    const options = {
+        method:"POST",
+        auth: "zzets:4efb331e912e8d969ac1d6a9026f0901-us17",
+        body: data
+    }
+    
+    
+    const request = https.request(url, options, (response) => {
+
+                let data = '';
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const result = JSON.parse(data);
+            res.send(result);
+            
+        if(response.error_count === 0){
+            res.sendFile(__dirname +"/succes.html")
+        }else{
+            res.sendFile(__dirname +"/failure.html")
+        }
+        });
+
+        });
+      
+      request.write(jsonData);
+      request.end();
+
+});
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log("server is runing");
+});
